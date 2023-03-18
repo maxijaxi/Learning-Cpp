e8afbb9372f29535941a88e13934c23fbde9d6c1
Maxis <maxi.schuster@icloud.com>
2023-03-18 16:53:51 +0100

    Finished options

--- Calculator/main.cpp
+++ Calculator/main.cpp
@@ -1,58 +1,80 @@
 #include <iostream>
+#include <limits>
 using namespace std;
 
 int calcOption = 0;
 int calcSize = 0;
 
-void intro() {
-	cout << "Greetings user!" << endl << endl;
-	cout << "We are preparing everything, please wait a second..." << endl << endl;
-	for (int i = 0; i <= 100; i += 20) {
-		cout << i << "%" << endl;
+// input error handling
+int inputChecker(string text) {
+	int output = 0;
+	while (!(std::cin >> output)) {
+		std::cout << "Invalid input...please enter an integer!" << endl << endl << text;
+		std::cin.clear(); 
+		std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
 	}
-	cout << endl;
-
-	for (int i = 0; i <= 60; i++) {
-		cout << "-";
-	}
-	cout << endl << endl;
+	return output;
 }
 
+// setting options for calculation
 void options() {
-	int UncheckedOption = 0;
-	int UncheckedSize = 0;
+	cout << "Please select your operation:" << endl << endl;
+	cout << "1: addition" << endl;
+	cout << "2: subtraction" << endl;
+	cout << "3: multiplication" << endl;
+	cout << "4. division" << endl;
 
+	// check input
 	do {
-		cout << "Please select your operation:" << endl << endl;
-		cout << "1: addition" << endl;
-		cout << "2: subtraction" << endl;
-		cout << "3: multiplication" << endl;
-		cout << "4. division" << endl;
-
 		cout << endl << "Operation: ";
-		std::cin >> UncheckedOption;
+		int UncheckedOption = inputChecker("Operation: ");
 		cout << endl;
 
-		if (UncheckedOption > 4) {
-			cout << "Invalid option! Please try again..." << endl;
-		}
-		else {
+		if (UncheckedOption <= 4) {
 			calcOption = UncheckedOption;
 			break;
+		}
+		else {
+			cout << "Invalid option! Please try again..." << endl;
 		}
 	} while (true);
 
 	for (int i = 0; i <= 60; i++) {
 		cout << "-";
 	}
+
 	cout << endl << endl;
+	cout << "Thanks...now please enter the amount of numbers, that the operation should handle!" << endl;
 
-	cout << "Thanks...now please enter the ammout of numbers, that the operation should handle!" << endl;
-	std::cin >> UncheckedSize;
-
+	// check input
+	do {
+		cout << endl << "Amount: ";
+		int UncheckedSize = inputChecker("Amount: ");
+		cout << endl;
+		if (UncheckedSize == 0) {
+			cout << "Ok...I see what you did there! In this case, fuck you too!";
+			exit(0);
+		}
+		else if (UncheckedSize == 1) {
+			cout << "There is no point of taking a number and doing nothing with it..." << endl;
+			cout << "Try again, but use your god damn brain this time." << endl;
+		}
+		else {
+			calcSize = UncheckedSize;
+			break;
+		}
+	} while (true);
 }
 
 int main() {
-	intro();
+	// hello message
+	cout << "Greetings user!" << endl << endl;
+
+	for (int i = 0; i <= 60; i++) {
+		cout << "-";
+	}
+	cout << endl << endl;
+
+	// setting options for calculation
 	options();
 }
